10d2eaf5a6f8fcbc43b4d57de674722fa7e36f34
korrupterkurde <37101345+aaronkurda@users.noreply.github.com>
2018-11-07 14:30:31 +0100

    Gravitatio eingefügt, Planeten als Prefabs

---
+++ Assets/Scripts/Attractor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attractor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Attract(Attractor toAttract)
+    {
+        Rigidbody toAttractRb = toAttract.GetComponent<Rigidbody>();
+        Vector3 direction = this.transform.position - toAttract.transform.position;
+        float distance = direction.magnitude;
+
+        float forceMagnitude = toAttractRb.mass * this.GetComponent<Rigidbody>().mass / Mathf.Pow(distance, 2);
+        Vector3 force = direction.normalized * forceMagnitude;
+
+        toAttractRb.AddForce(force);
+    }
+}
