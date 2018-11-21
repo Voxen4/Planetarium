9e3316075861d59b178fb252d7ed49ba9b56c42e
Voxen4 <mi.keller@ostfalia.de>
2018-11-21 14:09:23 +0100

    Added Prefab Planets

---
+++ Assets/PlanetMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Rigidbody body = GetComponent<Rigidbody>();
+        body.AddForce(new Vector3(10,0,0));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
