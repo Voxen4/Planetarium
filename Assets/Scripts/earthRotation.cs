2e8a66dace849a3c9613e29850c62d0982ed37ff
Scantraxx123 <34422209+Scantraxx123@users.noreply.github.com>
2018-11-27 22:05:58 +0100

    Update earthRotation.cs

--- Assets/Scripts/earthRotation.cs
+++ Assets/Scripts/earthRotation.cs
@@ -1,20 +1,28 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class earthRotation : MonoBehaviour {
 
-    public float speed = 2f;
+    float PlanetRotateSpeed = -25.0F;
+    float OrbitSpeed = 10.0F;
 
 	// Use this for initialization
 	void Start () {
+
+       
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        transform.Rotate(0, speed, 0);
+        transform.Rotate(Vector3.up, Space.Self);
 
-	}
+        //transform.Rotate(transform.up * PlanetRotateSpeed * Time.deltaTime);
+        //transform.RotateAround(Vector3.zero, Vector3.up, OrbitSpeed * Time.deltaTime);
+
+
+
+    }
 }
