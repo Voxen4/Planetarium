678631166d76d613e9d2d435de5d2c0bfaf3440a
y123456 <aaronkurda@web.de>
2018-12-01 12:29:25 +0100

    Neues Sonnensystem angelegt. Alle Planeten eingefügt. Apohel Speed ist bis jetzt durschnittliche Oribtalgeschwindigkeit. Diameter hat bis jetzt einen Platzhalterwert.

--- Assets/Scripts/PlanetData.cs
+++ Assets/Scripts/PlanetData.cs
@@ -3,17 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlanetData : MonoBehaviour {
-    //Wir können leider diese größen Zahlen nicht im Transform oder im Rigidbody einzeigen lassen.
-    //Folgenden Umrechnungsfaktoren:
-    //Strecken [km] -> 10^6 [km] => Umrechungsfaktor von 10^-6 (10^6 km in der realität werden zu der Einheit 1 im Transform)
-    //Masse [kg] -> 10^22 [kg]
-    public double mass;             //Masse des Körpers in kg
-    public float diameter;          //Durchmesser des Körpers in km
-    public double apohelHeight;     //Höhe der Apoapsis in km
+    //Die Masse des Planeten in Erdenmassen. (Das reale >Gewicht in kg muss durch die Masse der Erde geteilt werden)
+    public double mass;             
+    //Der Durchmesser des Planeten. Sind bis jetzt nur platzhalter.
+    public float diameter;        
+    //Höhe des höchsten Punkts des Orbits in 10 Mio km. (147.000.000 -> 14,7)
+    public double apohelHeight;     
+    //Geschwidigkeit des Planeten. BIs jetzt nur Platzhalter (29290m -> 2,9m)
     public double apohelSpeed;      //Geschwindigkeit an Apoapsis in km/s#
-
+    //Der Planet um der das Objekt kreist. Im Sonnensystem ist das die Sonne (solange der Planet kein Mond ist)
     public GameObject sphereOfInfluence;
-
+    //Maximale und Minimale Distanz die der Planet auf seinem Orbit um den Stern erreicht hat. Wird im Mouse-Over angezeigt.
     private float maxDistance;
     private float minDistance;
 
