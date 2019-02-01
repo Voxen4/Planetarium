f88121fea1505614acceced702651506fe945293
Arbeitslaptop <F.Willrich@ostfalia.de>
2019-02-01 17:06:51 +0100

    Dict gefixed

--- Assets/Scripts/NasaData.cs
+++ Assets/Scripts/NasaData.cs
@@ -1,12 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class NasaData : MonoBehaviour
 {
     public Planet planet;
     public TextAsset asset;
-    private Dictionary<Date, Parsed> parsedPositions = new Dictionary<Date, Parsed>();
+    private Dictionary<short, Parsed> parsedPositions = new Dictionary<short, Parsed>();
     // Use this for initialization
 
     private void Awake()
@@ -17,27 +16,30 @@ public class NasaData : MonoBehaviour
         {
             string[] columns = item.Split(',');
             string[] date = columns[1].Split('-');
-            Parsed parsedLine = new Parsed()
+            if (date[1].Equals("01") && date[2].Equals("01"))
             {
-                date = new Date(short.Parse(date[0]), short.Parse(date[1]), short.Parse(date[2]) )
-                ,
-                X = double.Parse(columns[2])
-                ,
-                Y = double.Parse(columns[3])
-                ,
-                Z = double.Parse(columns[4])
-                ,
-                VX = double.Parse(columns[5])
-                ,
-                VY = double.Parse(columns[6])
-                ,
-                VZ = double.Parse(columns[7])
-            };
-            parsedPositions.Add(parsedLine.date,parsedLine);
+                Parsed parsedLine = new Parsed()
+                {
+                    date = new Date(short.Parse(date[0]))
+                    ,
+                    X = double.Parse(columns[2])
+                    ,
+                    Y = double.Parse(columns[3])
+                    ,
+                    Z = double.Parse(columns[4])
+                    ,
+                    VX = double.Parse(columns[5])
+                    ,
+                    VY = double.Parse(columns[6])
+                    ,
+                    VZ = double.Parse(columns[7])
+                };
+                parsedPositions.Add(parsedLine.date.Year, parsedLine);
+            }
         }
     }
 
-    public class Date : IEqualityComparer<Date>
+    public class Date
     {
         private short day;
         private short year;
@@ -45,10 +47,11 @@ public class NasaData : MonoBehaviour
 
         public Date(short _year = 1959, short _month = 1, short _day = 1)
         {
-            this.Year=_year;
+            this.Year = _year;
             this.Month = _month;
             this.Day = _day;
         }
+
 
         public short Year
         {
@@ -88,16 +91,6 @@ public class NasaData : MonoBehaviour
                 day = value;
             }
         }
-
-        public bool Equals(Date x, Date y)
-        {
-            return x.Year == y.Year && x.Month == y.Month && x.Day == y.Day;
-        }
-
-        public int GetHashCode(Date obj)
-        {
-            return obj.Year.GetHashCode() ^  obj.Month.GetHashCode() ^ obj.Day.GetHashCode();
-        }
     }
     public class Parsed
     {
@@ -105,17 +98,19 @@ public class NasaData : MonoBehaviour
         public double JDTDB, X, Y, Z, VX, VY, VZ, LT, RG, RR;
     }
 
-        public Parsed GetParsed(Date date)
+    public Parsed GetParsed(Date date)
     {
-        if(date == null)
+        if (date == null)
         {
             date = new Date();
         }
         Parsed parsedValue;
-        if (parsedPositions.ContainsKey(date))
+        if (parsedPositions.ContainsKey(date.Year))
         {
-            parsedPositions.TryGetValue(date,out parsedValue);
-            return parsedValue;
+
+            return null;
+            //parsedPositions.TryGetValue(date.Year, out parsedValue);
+            //return parsedValue;
         }
         else
         {
