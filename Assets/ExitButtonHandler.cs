fdf56fdea7d445dd93da8be367b2b2af30f2944f
Arbeitslaptop <F.Willrich@ostfalia.de>
2019-02-05 17:26:33 +0100

    Exit Button eingebaut

---
+++ Assets/ExitButtonHandler.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitButtonHandler : MonoBehaviour
+{
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+}
