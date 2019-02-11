e067333900d812d9acb2951d04197de579ecdb46
Arbeitslaptop <F.Willrich@ostfalia.de>
2019-02-11 01:09:20 +0100

    Etwas aufräumen und Kommentieren

--- Assets/Scripts/StartExitButtonHandler.cs
+++ Assets/Scripts/StartExitButtonHandler.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class StartExitButtonHandler : MonoBehaviour
 {
+    
+    ///  Exit Button zum schließen der APP
     public void ExitButton()
     {
         Application.Quit();
     }
 
+    /// Starten der Scene über den StartKnopf, Pausemenü wird beendet und Startkoordinaten übergeben
     public void StartButton()
     {
         ((GameMgr)GameMgr.Instance).TogglePauseMenu();
